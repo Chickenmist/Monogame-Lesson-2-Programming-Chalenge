81b0cfabba255bc1a72fa315fa150ac5300a74f0
Chickenmist <wpimlatt@gmail.com>
2024-04-15 14:41:26 -0400

    Add project files.

---
+++ Monogame Lesson 2 Prgramming Chalenge/Game1.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame_Lesson_2_Prgramming_Chalenge
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D tileTexture;
+
+        Rectangle tile;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            _graphics.PreferredBackBufferWidth = 640;
+            _graphics.PreferredBackBufferHeight = 640;
+            _graphics.ApplyChanges();
+
+            tile = new Rectangle(0, 0, 80, 80);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            tileTexture = Content.Load<Texture2D>("rectangle");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+
+            for (int i = 0; i < 640 - tile.Width; i += tile.Width * 2)
+            {
+                for (int j = 0; j < 640 - tile.Height; j += tile.Height * 2)
+                {
+                    _spriteBatch.Draw(tileTexture, new Vector2(i,j), Color.White);
+                }
+            }
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Monogame Lesson 2 Prgramming Chalenge/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Monogame_Lesson_2_Prgramming_Chalenge.Game1();
+game.Run();
